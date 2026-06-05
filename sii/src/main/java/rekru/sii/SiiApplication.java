32520ebaed4aa22ce2efb7e9739b4ccfa907ea40
wzelechowski <ZeloWiktor@wp.pl>
2026-06-05 16:44:30 +0200

    project dependencies and package structure

---
+++ sii/src/main/java/rekru/sii/SiiApplication.java
@@ -0,0 +1,13 @@
+package rekru.sii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiiApplication.class, args);
+    }
+
+}
---
+++ sii/src/test/java/rekru/sii/SiiApplicationTests.java
@@ -0,0 +1,13 @@
+package rekru.sii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
